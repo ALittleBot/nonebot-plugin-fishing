e1370fd8c25f711c0e74eb8795da0a16e87de8e3
泠风寒声 <160833462+C14H22O@users.noreply.github.com>
2024-03-09 12:30:04 +0800

    更改 orm

--- nonebot_plugin_fishing/data_source.py
+++ nonebot_plugin_fishing/data_source.py
@@ -2,22 +2,16 @@ from nonebot import get_plugin_config
 from nonebot import require
 from nonebot.log import logger
 
-require("nonebot_plugin_localstore")  # noqa
-import nonebot_plugin_localstore as store
-
 import random
 import time
 import json
-from os.path import exists
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy import create_engine
 from sqlalchemy import select
 from sqlalchemy import update
+require("nonebot_plugin_orm")  # noqa
+from nonebot_plugin_orm import get_session
 
 from .config import Config
-from .model import Base
-from .model import FishingRecord, FishingBackpack
+from .model import FishingRecord
 
 plugin_config = get_plugin_config(Config)
 
@@ -32,53 +26,26 @@ def choice() -> tuple:
     return choices[0][0], choices[0][1]
 
 
-data_path = str(store.get_data_file("fishing", "data.db"))
-async_engine = create_async_engine("sqlite+aiosqlite:///" + data_path)
-async_session = sessionmaker(class_=AsyncSession, bind=async_engine)  # noqa
-engine = create_engine("sqlite:///" + data_path)
-
-
-def init_database():
-    logger.info("正在初始化数据库…")
-    Base.metadata.create_all(engine)
-
-
-if not exists(data_path):
-    init_database()
-else:
-    logger.info(f"使用 {data_path} 的数据库")
-
-
 async def get_fishing(user_id: str) -> bool:
     time_now = int(time.time())
-    fishing_limit = plugin_config.fishing_limit
-    async with async_session() as session:
+    session = get_session()
+    async with session.begin():
         records = await session.execute(select(FishingRecord))
         for record in records.scalars():
             if record.user_id == user_id:
                 if record.time >= time_now:
                     return False
-                user_update = update(FishingRecord).where(FishingRecord.user_id == user_id).values(
-                    time=time_now + fishing_limit,
-                    frequency=record.frequency + 1
-                )
-                await session.execute(user_update)
-                await session.commit()
                 return True
         else:
-            new_record = FishingRecord(
-                user_id=user_id,
-                time=time_now + fishing_limit,
-                frequency=1
-            )
-            session.add(new_record)
-            await session.commit()
             return True
 
 
 async def save_fish(user_id: str, fish_name: str) -> None:
-    async with async_session() as session:
-        records = await session.execute(select(FishingBackpack))
+    time_now = int(time.time())
+    fishing_limit = plugin_config.fishing_limit
+    session = get_session()
+    async with session.begin():
+        records = await session.execute(select(FishingRecord))
         for record in records.scalars():
             if record.user_id == user_id:
                 loads_fishes = json.loads(record.fishes)
@@ -87,26 +54,32 @@ async def save_fish(user_id: str, fish_name: str) -> None:
                 except KeyError:
                     loads_fishes[fish_name] = 1
                 dump_fishes = json.dumps(loads_fishes)
-                user_update = update(FishingBackpack).where(FishingBackpack.user_id == user_id).values(
+                user_update = update(FishingRecord).where(FishingRecord.user_id == user_id).values(
+                    time=time_now + fishing_limit,
+                    frequency=record.frequency + 1,
                     fishes=dump_fishes
                 )
                 await session.execute(user_update)
                 await session.commit()
                 return
-        data = {
-            fish_name: 1
-        }
-        dump_fishes = json.dumps(data)
-        new_record = FishingBackpack(
-            user_id=user_id,
-            fishes=dump_fishes
-        )
-        session.add(new_record)
-        await session.commit()
+        else:
+            data = {
+                fish_name: 1
+            }
+            dump_fishes = json.dumps(data)
+            new_record = FishingRecord(
+                user_id=user_id,
+                time=time_now + fishing_limit,
+                frequency=1,
+                fishes=dump_fishes
+            )
+            session.add(new_record)
+            await session.commit()
 
 
 async def get_stats(user_id: str) -> str:
-    async with async_session() as session:
+    session = get_session()
+    async with session.begin():
         fishing_records = await session.execute(select(FishingRecord))
         for fishing_record in fishing_records.scalars():
             if fishing_record.user_id == user_id:
@@ -122,8 +95,9 @@ def print_backpack(backpack: dict) -> str:
 
 
 async def get_backpack(user_id: str) -> str:
-    async with async_session() as session:
-        fishes_records = await session.execute(select(FishingBackpack))
+    session = get_session()
+    async with session.begin():
+        fishes_records = await session.execute(select(FishingRecord))
         for fishes_record in fishes_records.scalars():
             if fishes_record.user_id == user_id:
                 load_fishes = json.loads(fishes_record.fishes)
--- nonebot_plugin_fishing/model.py
+++ nonebot_plugin_fishing/model.py
@@ -1,21 +1,12 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
-
-
-class FishingRecord(Base):
-    __tablename__ = "fishing_record"
-
-    id = Column(Integer, primary_key=True)
-    user_id = Column(String)
-    time = Column(Integer)
-    frequency = Column(Integer)
+from nonebot import require
+require("nonebot_plugin_orm")  # noqa
+from nonebot_plugin_orm import Model
+from sqlalchemy.orm import Mapped, mapped_column
 
 
-class FishingBackpack(Base):
-    __tablename__ = "fishing_backpack"
-
-    id = Column(Integer, primary_key=True)
-    user_id = Column(String)
-    fishes = Column(String)
+class FishingRecord(Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[str]
+    time: Mapped[int]
+    frequency: Mapped[int]
+    fishes: Mapped[str]
