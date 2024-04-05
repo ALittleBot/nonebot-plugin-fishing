115a1171fe5d64713646e149aef42dc26948ead6
C14H22O <160833462+C14H22O@users.noreply.github.com>
2024-04-05 19:11:30 +0800

    添加数据库字符串长度限制 #2

--- nonebot_plugin_fishing/model.py
+++ nonebot_plugin_fishing/model.py
@@ -1,11 +1,12 @@
 from nonebot_plugin_orm import Model
+from sqlalchemy import String, TEXT
 from sqlalchemy.orm import Mapped, mapped_column
 
 
 class FishingRecord(Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[str]
+    user_id: Mapped[str] = mapped_column(String(32))
     time: Mapped[int]
     frequency: Mapped[int]
-    fishes: Mapped[str]
+    fishes: Mapped[str] = mapped_column(TEXT)
     coin: Mapped[int]
