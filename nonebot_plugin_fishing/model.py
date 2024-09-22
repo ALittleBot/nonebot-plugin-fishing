bc957f940a8bcc7d6edeb19cfc6de07b8217a786
ALittleBot <160833462+ALittleBot@users.noreply.github.com>
2024-09-22 17:15:23 +0800

    可能修复了数据库升级错误的问题

--- nonebot_plugin_fishing/model.py
+++ nonebot_plugin_fishing/model.py
@@ -9,12 +9,12 @@ class FishingRecord(Model):
     time: Mapped[int]
     frequency: Mapped[int]
     fishes: Mapped[str] = mapped_column(TEXT)
-    special_fishes: Mapped[str] = mapped_column(TEXT)
+    special_fishes: Mapped[str] = mapped_column(TEXT, default="{}")
     coin: Mapped[int] = mapped_column(default=0)
-    achievements: Mapped[str] = mapped_column(TEXT)
+    achievements: Mapped[str] = mapped_column(TEXT, default="[]")
 
 
 class SpecialFishes(Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[str] = mapped_column(String(32))
-    fish: Mapped[str] = mapped_column(TEXT)
+    fish: Mapped[str] = mapped_column(TEXT, default="{}")
